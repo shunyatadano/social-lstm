0a1a52fe17b6ae3787fe1d6d2fcfb9f47802b93d
eeveetamlab <eeveetamlab@gmail.com>
2024-10-14 17:23:48 +0900

    add codes for dataset operations

--- src_thor-magni/csv2txt.py
+++ src_thor-magni/csv2txt.py
@@ -2,15 +2,20 @@ import pandas as pd
 
 # CSVファイルの読み込み
 # csv_file = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/train/interpolated_data.csv'
-# csv_file = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/val/interpolated_data.csv'
-csv_file = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/test/test_interpolated.csv'
+csv_file = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/val/interpolated_data.csv'
+# csv_file = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/test/test_interpolated.csv'
 df = pd.read_csv(csv_file)
 
 # 必要なカラムの抽出と順番の入れ替え
 df = df[['frame_id', 'ag_id', 'y', 'x']]  # 'z'が不要な場合は除く
+# ag_idの始まりを1にする
+df['ag_id'] = df['ag_id'] + 1
+print(f"xrange: {df['x'].min()} to {df['x'].max()}")
+print(f"yrange: {df['y'].min()} to {df['y'].max()}")
 
 # タブ区切り形式でtxtファイルとして保存
-# save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/train/interpolated_data.txt'
-# save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/val/interpolated_data.txt'
-save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/test/interpolated_data.txt'
-df.to_csv(save_path, sep='\t', index=False, header=False)
+# save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/train/Scenario_1.txt'
+save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/val/Scenario_1.txt'
+# save_path = '/home/eevee/social-lstm/thor-magni/tm_png_merge/Scenario_1/txt/test/Scenario_1.txt'
+df.to_csv(save_path, sep=' ', index=False, header=False)
+# df.to_csv(save_path, sep='\t', index=False, header=False)
